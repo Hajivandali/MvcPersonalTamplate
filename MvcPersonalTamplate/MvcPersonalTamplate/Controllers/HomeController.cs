b9b2a339e0f1b85b48b47fa6a38dc0f6baa00f09
Ali <alihajivand14@gmail.com>
2022-07-08 00:48:10 +0430

    add tmplate full

--- MvcPersonalTamplate/MvcPersonalTamplate/Controllers/HomeController.cs
+++ MvcPersonalTamplate/MvcPersonalTamplate/Controllers/HomeController.cs
@@ -6,11 +6,9 @@ namespace MvcPersonalTamplate.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController()
         {
-            _logger = logger;
         }
 
         public IActionResult Index()
@@ -18,11 +16,10 @@ namespace MvcPersonalTamplate.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Contact()
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
