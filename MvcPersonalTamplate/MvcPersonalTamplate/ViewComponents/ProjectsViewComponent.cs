c919d51b4b66284c44e2886377820186de513d2d
Ali <alihajivand14@gmail.com>
2022-07-12 13:42:30 +0430

    1.0

---
+++ MvcPersonalTamplate/MvcPersonalTamplate/ViewComponents/ProjectsViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace MvcPersonalTamplate.ViewComponents
+
+{
+    public class ProjectsViewComponent : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+
+
+    }
+}
