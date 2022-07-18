145a49d451bf794b279b6206aa991e56d772ff9f
Ali <alihajivand14@gmail.com>
2022-07-18 14:42:09 +0430

    a

--- MvcPersonalTamplate/MvcPersonalTamplate/ViewComponents/ProjectsViewComponent.cs
+++ MvcPersonalTamplate/MvcPersonalTamplate/ViewComponents/ProjectsViewComponent.cs
@@ -11,9 +11,14 @@ namespace MvcPersonalTamplate.ViewComponents
         {
             var projects = new List<Project>
             {
-                new Project()
+                new Project(1,"taxi","description taxi app","HajivandAli"),
+                new Project(2,"modirit","description modirit app","HajivandAli"),
+                new Project(3,"foodapp","description foodapp app","HajivandAli"),
+                new Project(4,"lunch","description lunch app","HajivandAli"),
+
+
             };
-                return View("_Projects");
+                return View("_Projects", projects);
         }
 
 
