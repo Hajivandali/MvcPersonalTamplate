0b80749e6c09425fd59932a0d6b4a45bfe8da1e0
Ali <alihajivand14@gmail.com>
2022-07-19 17:43:47 +0430

    add articles

---
+++ MvcPersonalTamplate/MvcPersonalTamplate/ViewComponents/ArticlesViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPersonalTamplate.Models;
+namespace MvcPersonalTamplate.ViewComponents
+{
+    public class ArticlesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var articles = new List<Articles>
+            {
+                new Articles(1,"تست شماره 1","پروژه تست شماره 1 ","blog-post-thumb-card-1.jpg"),
+                new Articles(2,"تست شماره 2","پروژه تست شماره 1 ","blog-post-thumb-card-2.jpg"),
+                new Articles(3,"تست شماره 3","پروژه تست شماره 1 ","blog-post-thumb-card-3.jpg"),
+                new Articles(4,"تست شماره 4","پروژه تست شماره 1 ","blog-post-thumb-card-4.jpg"),
+
+            };
+            return View("_Articles",articles);
+        }
+    }
+}
--- MvcPersonalTamplate/MvcPersonalTamplate/ViewComponents/ProjectsViewComponent.cs
+++ MvcPersonalTamplate/MvcPersonalTamplate/ViewComponents/ProjectsViewComponent.cs
@@ -11,10 +11,10 @@ namespace MvcPersonalTamplate.ViewComponents
         {
             var projects = new List<Project>
             {
-                new Project(1,"taxi","description taxi app","HajivandAli"),
-                new Project(2,"modirit","description modirit app","HajivandAli"),
-                new Project(3,"foodapp","description foodapp app","HajivandAli"),
-                new Project(4,"lunch","description lunch app","HajivandAli"),
+                new Project(1,"taxi","description taxi app","project-1.jpg","HajivandAli"),
+                new Project(2,"modirit","description modirit app","project-2.jpg","HajivandAli"),
+                new Project(3,"foodapp","description foodapp app","project-3.jpg","HajivandAli"),
+                new Project(4,"lunch","description lunch app","project-4.jpg","HajivandAli"),
 
 
             };
