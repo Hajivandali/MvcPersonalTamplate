a067b9f0a9b4d926f0812b4264664e94edb5c4be
Ali <alihajivand14@gmail.com>
2022-07-26 13:31:18 +0430

    validation form

--- MvcPersonalTamplate/MvcPersonalTamplate/Models/ContactForm.cs
+++ MvcPersonalTamplate/MvcPersonalTamplate/Models/ContactForm.cs
@@ -1,8 +1,15 @@
-﻿namespace MvcPersonalTamplate.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcPersonalTamplate.Models
 {
     public class ContactForm
     {
+        [Required(ErrorMessage ="فیلد اجباری است ")]
+        [MinLength(3)]
+        [MaxLength(100)]
         public string Name { get; set; }
+        [Required(ErrorMessage = "فیلد اجباری است ")]
+        [EmailAddress(ErrorMessage ="مقدار وارد شده صحیح نیست")]
         public string Email { get; set; }
         public string Message { get; set; }
         public string Service { get; set; }
